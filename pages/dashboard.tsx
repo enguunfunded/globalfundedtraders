a2224903190572da81fb7dff3d641aec32c61cde
enguunfunded <jagaa_tsg@yahoo.com>
2025-05-21 19:15:25 +0800

    Delete pages directory

--- pages/dashboard.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <main className="min-h-screen text-white bg-gray-900 p-10 text-center">
-      <h1 className="text-4xl font-bold mb-4">📊 User Dashboard</h1>
-      <p className="text-gray-300">Энд таны account, challenge статус, санхүүжилт харагдана.</p>
-    </main>
-  );
-}
